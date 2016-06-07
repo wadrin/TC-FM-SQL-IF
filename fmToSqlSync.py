6c4d5e306bf627037d0a3230568935c5d6ff10b5
wadrin <jaap@kroesschell-ictconsulting.ch>
2016-06-07 21:35:57 +0700

    fm->sql import works, bypass duplicates, no datetime yet, cleaned

--- fmToSqlSync.py
+++ fmToSqlSync.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 #
 # filename fmToSqlSync.py
-# version 0.33 - June 6, 2016 by <Jaap at kroesschell-ictconsulting.ch>
+# version 0.35 - June 7, 2016 by <Jaap at kroesschell-ictconsulting.ch>
 # Objective read costomer records from export set and save into sql db
 # 
 #
@@ -158,64 +158,3 @@ def run():
 
 
 run()
-
-
-# ['Customer_ID', 'Customers_Village_Sale', 'Date_Payment_10', 'Date_Payment_11', 'Date_Payment_12', 'Date_Payment_1_Initial', 'Date_Payment_2', 'Date_Payment_3',
-# 'Date_Payment_4', 'Date_Payment_5', 'Date_Payment_6', 'Date_Payment_7', 'Date_Payment_8', 'Date_Payment_9', 'Date_Payment_Adjustment_1', 'Date_Payment_Adjustment_2', 
-# 'Date_Scheduled_Payment_2', 'Date_Scheduled_Payment_3', 'Date_Scheduled_Payment_4', 'Date_Scheduled_Payment_5', 'Date_Scheduled_Payment_6', 'Date_Scheduled_Payment_7', 
-# 'Date_Scheduled_Payment_8', 'Date_Scheduled_Payment_9', 'District_Reference_Village_Sales', 'MODID', 'Notes', 'Notes_Adjustment_1', 'Notes_Adjustment_2', 'Payment_10', 
-# 'Payment_10_Collector_ID', 'Payment_10_Receipt_No', 'Payment_11', 'Payment_11_Collector_ID', 'Payment_11_Receipt_No', 'Payment_12', 'Payment_12_Collector_ID', 
-# 'Payment_12_Receipt_No', 'Payment_1_Initial', 'Payment_2', 'Payment_2_Collector_ID', 'Payment_2_Receipt_No', 'Payment_3', 'Payment_3_Collector_ID', 'Payment_3_Receipt_No', 
-# 'Payment_4', 'Payment_4_Collector_ID', 'Payment_4_Receipt_No', 'Payment_5', 'Payment_5_Collector_ID', 'Payment_5_Receipt_No', 'Payment_6', 'Payment_6_Collector_ID', 
-# 'Payment_6_Receipt_No', 'Payment_7', 'Payment_7_Collector_ID', 'Payment_7_Receipt_No', 'Payment_8', 'Payment_8_Collector_ID', 'Payment_8_Receipt_No', 'Payment_9', 
-# 'Payment_9_Collector_ID', 'Payment_9_Receipt_No', 'Payment_Adjustment_1', 'Payment_Adjustment_1_Collector_ID', 'Payment_Adjustment_1_Receipt_No', 'Payment_Adjustment_2', 
-# 'Payment_Adjustment_2_Collector_ID', 'Payment_Adjustment_2_Receipt_No', 'Payment_Due_Collections', 'Payment_Outstanding', 'Payment_Scheduled_2', 'Payment_Scheduled_3', 
-# 'Payment_Scheduled_4', 'Payment_Scheduled_5', 'Payment_Scheduled_6', 'Payment_Scheduled_7', 'Payment_Scheduled_8', 'Payment_Scheduled_9', 'Province_Reference_Village_Sales',
-# 'RECORDID', 'Receipt', 'Receipt_ID', 'Sales_Agent_ID', 'Village_Reference_Village_Sales', 
-# '__class__', '__delattr__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__init_dict__', '__iter__', '__modified__', '__module__', 
-# '__new2old__', '__new__', '__old2new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', 
-# '_modified', 'get']
-
-
-# (entry.Customers_Village_Sale)
-# ['District_ID', 'Name_Full_Bilingual', 'Notes', 'Phone_1', 'Province_ID', 'Ship_To_Street_Lao', 'Village_ID',
-#  '__class__', '__delattr__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__init_dict__', '__iter__', '__modified__', '__module__',
-#  '__new2old__', '__new__', '__old2new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', 
-#  '_modified', 'get']
-
-
-# (entry.District_Reference_Village_Sales)
-# ['District_Name_English', 'District_Name_Lao', '__class__', '__delattr__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__init_dict__', \
-# '__iter__', '__modified__', '__module__', '__new2old__', '__new__', '__old2new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__sizeof__', \
-# '__slots__', '__str__', '__subclasshook__', '_modified', 'get']
-
-
-# (entry.Province_Reference_Village_Sales)
-# ['Province_Name_Bilingual', 'Province_Name_English', 'Province_Name_Lao', '__class__', '__delattr__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', \
-# '__init__', '__init_dict__', '__iter__', '__modified__', '__module__', '__new2old__', '__new__', '__old2new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', \
-# '__setitem__', '__sizeof__', '__slots__', '__str__', '__subclasshook__', '_modified', 'get']
-
-
-# (entry.Village_Reference_Village_Sales)
-# ['Village_Name_English', 'Village_Name_Lao', '__class__', '__delattr__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__init_dict__', \
-# '__iter__', '__modified__', '__module__', '__new2old__', '__new__', '__old2new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setitem__', '__sizeof__',\
-#  '__slots__', '__str__', '__subclasshook__', '_modified', 'get']
-
-
-			# print 'Customer ID', entry.Customer_ID, int(entry.Customer_ID), type(entry.Customer_ID)
-			# print 'name', entry.Customers_Village_Sale.Name_Full_Bilingual, entry.Customers_Village_Sale.Name_Full_Bilingual.decode('utf-8')
-			# print 'province_id', entry.Customers_Village_Sale.Province_ID,
-			# print 'province_la_en', entry.Province_Reference_Village_Sales.Province_Name_Bilingual,
-			# print 'district_id', entry.Customers_Village_Sale.District_ID,
-			# print 'district_la', entry.District_Reference_Village_Sales.District_Name_Lao,
-			# print 'village_id', entry.Customers_Village_Sale.Village_ID,
-			# print 'village_la', entry.Village_Reference_Village_Sales.Village_Name_Lao,
-			# print 'sub_unit',
-			# print 'latitude',
-			# print 'longitude',
-			# print 'phone_1', entry.Customers_Village_Sale.Phone_1,
-			# print 'phone_2',
-			# print 'notes', entry.Customers_Village_Sale.Notes,
-			# print 'collector_id',
-			# print 'sync_datetime',
-			# print 'update_datetime'
