baa5be3b4b7a35e9a5bd9cd832910548ffc68b69
wadrin <jaap@kroesschell-ictconsulting.ch>
2016-06-08 14:04:39 +0700

    populate aws, fixed sub-unit, sync_timedate.

--- fmToSqlSync.py
+++ fmToSqlSync.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 #
 # filename fmToSqlSync.py
-# version 0.36 - June 8, 2016 by <Jaap at kroesschell-ictconsulting.ch>
-# Objective read costomer records from export set and populate sql db
+# version 0.37 - June 8, 2016 by <Jaap at kroesschell-ictconsulting.ch>
+# Objective read customer records from export set and populate sql db
 # 
 #
 # python 2.7 
@@ -111,15 +111,16 @@ def getFromFmToSql():
 	steps = 50
 	lastRecord = firstRecord + steps
 
-	while play:
-		myList = list(range(firstRecord,lastRecord))
+	while play: # runs as long as play is True
+		myList = list(range(firstRecord,lastRecord)) # update range in list
 		lastRecord = lastRecord + steps
 		firstRecord = firstRecord + steps
-		# print "------  from ", firstRecord, " to ", lastRecord," ------"
+		# print "------  from ", firstRecord, " to ", lastRecord," ------" # show import progress with record number range
 		# play = False
 		myList = list(range(firstRecord,lastRecord))	# Look for the x records, range([start], stop[, step
 		# print "list", myList
-		results = fm.doFindQuery({'_Customer_ID': myList, })
+		results = fm.doFindQuery({'_Customer_ID': myList, }) # put all customers in results
+		print "results is of type :" type(results)
 		importedRecords = len(results)	
 		if importedRecords > 0 :
 			play = True
@@ -132,7 +133,9 @@ def getFromFmToSql():
 			# print '.',
 			# print datetime(now)
 			# print "entry.Customer_ID ", type(int(entry.Customer_ID)), entry.Customer_ID
-
+			# table for aws `WADRIN_dev`
+			# table for local env `test_database`
+			# print dir(entry.Customers_Village_Sale)
 			try:
 				cursor.execute("""INSERT IGNORE INTO `WADRIN_dev`.`customer` (`Customer_ID`, `name`, \
 					`province_id`,`province_la_en`, `district_id`, `district_la`, `village_id`, `village_la`, \
@@ -146,29 +149,31 @@ def getFromFmToSql():
 				 		entry.District_Reference_Village_Sales.District_Name_Lao, 
 				 		hasValue(entry.Customers_Village_Sale.Village_ID),
 				 		entry.Village_Reference_Village_Sales.Village_Name_Lao, 
-				 		None, 
-				 		None, 
-				 		None, 
+				 		entry.Customers_Village_Sale.Ship_To_Street_Lao, #sub_unit
+				 		None, # latitude
+				 		None, # longitude
 				 		entry.Customers_Village_Sale.Phone_1, 
-				 		None, 
+				 		None, # phone_2
 				 		entry.Customers_Village_Sale.Notes, 
-				 		None, 
-				 		None, 
-				 		stamp ))
+				 		None, # collector_id
+				 		stamp, # sync_datetime
+				 		None )) # update_datetime
+				# print entry.Customers_Village_Sale.Ship_To_Street_Lao # debug display field content
 				db.commit()
-				if int(entry.Customer_ID) > 35654: play = False
+				if int(entry.Customer_ID) > 35654: play = False # use 35654 for full job, 33580 for debug 
 			except IOError as e:
 				print "Customer_ID ", entry.Customer_ID , "sql transaction not succesful ", e
 				db.rollback()
 		else:
-			print  "last record imported.",
+			print  ".",
+	print  "last record imported. 
 	db.close()	# Disconnect from sql server
 	return
 
 
 def run():
-	# initSqldb()
-	# getFromFmToSql()
+	initSqldb()
+	getFromFmToSql()
 	return
 
 
@@ -176,3 +181,26 @@ run()
 
 # local db transfer time real	2m22.803s 
 # aws db transfer time real	6m21.603s
+
+# dir(entry) \
+# 'Customer_ID', 'Customers_Village_Sale', 'Date_Payment_10', 'Date_Payment_11', 'Date_Payment_12', \
+# 'Date_Payment_1_Initial', 'Date_Payment_2', 'Date_Payment_3', 'Date_Payment_4', 'Date_Payment_5', \
+# 'Date_Payment_6', 'Date_Payment_7', 'Date_Payment_8', 'Date_Payment_9', 'Date_Payment_Adjustment_1', \
+# 'Date_Payment_Adjustment_2', 'Date_Scheduled_Payment_2', 'Date_Scheduled_Payment_3', 'Date_Scheduled_Payment_4', \
+# 'Date_Scheduled_Payment_5', 'Date_Scheduled_Payment_6', 'Date_Scheduled_Payment_7', 'Date_Scheduled_Payment_8', \
+# 'Date_Scheduled_Payment_9', 'District_Reference_Village_Sales', 'MODID', 'Notes', 'Notes_Adjustment_1', \
+# 'Notes_Adjustment_2', 'Payment_10', 'Payment_10_Collector_ID', 'Payment_10_Receipt_No', 'Payment_11', \
+# 'Payment_11_Collector_ID', 'Payment_11_Receipt_No', 'Payment_12', 'Payment_12_Collector_ID', \
+# 'Payment_12_Receipt_No', 'Payment_1_Initial', 'Payment_2', 'Payment_2_Collector_ID', 'Payment_2_Receipt_No', \
+# 'Payment_3', 'Payment_3_Collector_ID', 'Payment_3_Receipt_No', 'Payment_4', 'Payment_4_Collector_ID', \
+# 'Payment_4_Receipt_No', 'Payment_5', 'Payment_5_Collector_ID', 'Payment_5_Receipt_No', 'Payment_6', \
+# 'Payment_6_Collector_ID', 'Payment_6_Receipt_No', 'Payment_7', 'Payment_7_Collector_ID', 'Payment_7_Receipt_No', \
+# 'Payment_8', 'Payment_8_Collector_ID', 'Payment_8_Receipt_No', 'Payment_9', 'Payment_9_Collector_ID', \
+# 'Payment_9_Receipt_No', 'Payment_Adjustment_1', 'Payment_Adjustment_1_Collector_ID', 'Payment_Adjustment_1_Receipt_No', \
+# 'Payment_Adjustment_2', 'Payment_Adjustment_2_Collector_ID', 'Payment_Adjustment_2_Receipt_No', 'Payment_Due_Collections', \
+# 'Payment_Outstanding', 'Payment_Scheduled_2', 'Payment_Scheduled_3', 'Payment_Scheduled_4', 'Payment_Scheduled_5', \
+# 'Payment_Scheduled_6', 'Payment_Scheduled_7', 'Payment_Scheduled_8', 'Payment_Scheduled_9', 'Province_Reference_Village_Sales', \
+# 'RECORDID', 'Receipt', 'Receipt_ID', 'Sales_Agent_ID', 'Village_Reference_Village_Sales'
+
+# dir(entry.Customers_Village_Sale)
+# 'District_ID', 'Name_Full_Bilingual', 'Notes', 'Phone_1', 'Province_ID', 'Ship_To_Street_Lao', 'Village_ID'
